1e589bef7a12431673e6a7a40433da106c771bbe
nicoleabril <nicole.abrilc@ucuenca.edu.ec>
2024-07-06 16:16:03 -0500

    Creacion de control

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+
+const Home = () => {
+
+  return (
+    <div>
+      <div>
+        <p>Proyecto de IoT</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
