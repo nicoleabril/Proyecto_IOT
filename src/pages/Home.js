9b00422d73c70439ca52a14fda173fc4a96b2dc8
nicoleabril <nicole.abrilc@ucuenca.edu.ec>
2024-07-06 18:13:43 -0500

    cambios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../assets/styles/Home.css';
+import Confetti from 'react-confetti';
 
 const Home = () => {
+
+    const [showConfetti, setShowConfetti] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShowConfetti(prev => !prev);
+    }, 5000); // Alterna cada 5 segundos
+
+    return () => clearInterval(interval); // Limpiar el intervalo al desmontar el componente
+  }, []);
+
   return (
     <div className="home-container">
-      <h1 className="title">Proyecto IoT</h1>
-      <p className="subtitle">Sistemas Distribuidos</p>
-      <a href="/ControlActividades"><button className="try-button">Probar</button></a>
+        {showConfetti && <Confetti />}
+        <h1 className="title">Proyecto IoT</h1>
+        <p className="subtitle">Sistemas Distribuidos</p>
+        <a href="/ControlActividades"><button className="try-button">Probar</button></a>
     </div>
   );
 }
