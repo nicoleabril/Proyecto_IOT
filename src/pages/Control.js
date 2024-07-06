f7d84ca1a5f709b12c2c86105dbfe4b93cdf4808
cami-vgs01 <camila.granda@ucuenca.edu.ec>
2024-07-06 18:18:16 -0500

    modificaciones

--- src/pages/Control.js
+++ src/pages/Control.js
@@ -1,39 +1,41 @@
 import React, { useState } from 'react';
-import { Button, Container, Typography } from '@mui/material';
+import { Button, Container, Typography, Slider, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 import { motion } from 'framer-motion';
 import Confetti from 'react-confetti';
 import '../assets/styles/Control.css';
 
 const Control = () => {
     const [focoEncendido, setFocoEncendido] = useState(false);
+    const [intensidadFoco, setIntensidadFoco] = useState(50);
     const [ventiladorEncendido, setVentiladorEncendido] = useState(false);
     const [sensorTemperatura, setSensorTemperatura] = useState(false);
-    const [showConfetti, setShowConfetti] = useState(false);
+    const [showDialog, setShowDialog] = useState(false);
+    const [circleColor, setCircleColor] = useState('#ccc'); // Estado inicial para el color del círculo
 
     const handleFocoClick = () => {
         setFocoEncendido(!focoEncendido);
-        setShowConfetti(true);
-        setTimeout(() => setShowConfetti(false), 3000); // Confetti se muestra por 3 segundos
+        if (!focoEncendido) {
+            setShowDialog(true);
+            setTimeout(() => {
+                setFocoEncendido(false);
+            }, 1000);
+        }
     };
 
-    const handleVentiladorClick = () => {
-        setVentiladorEncendido(!ventiladorEncendido);
-        setShowConfetti(true);
-        setTimeout(() => setShowConfetti(false), 3000); // Confetti se muestra por 3 segundos
+    const handleCloseDialog = () => {
+        setShowDialog(false);
     };
 
-    const handleSensorClick = () => {
-        setSensorTemperatura(!sensorTemperatura);
-        setShowConfetti(true);
-        setTimeout(() => setShowConfetti(false), 3000); // Confetti se muestra por 3 segundos
+    const handleChangeIntensidadFoco = (event, newValue) => {
+        setIntensidadFoco(newValue);
     };
 
     return (
         <div className="Control">
-            {showConfetti && <Confetti />}
+            <Confetti />
             <Container className="App" maxWidth="md">
                 <Typography variant="h2" gutterBottom>
-                    Control de Animaciones
+                    Control de Actividades
                 </Typography>
                 <div className="buttons-container">
                     <motion.div
@@ -55,10 +57,11 @@ const Control = () => {
                         <Button
                             variant="contained"
                             color={ventiladorEncendido ? "secondary" : "primary"}
-                            onClick={handleVentiladorClick}
+                            onClick={() => setVentiladorEncendido(!ventiladorEncendido)}
                         >
                             {ventiladorEncendido ? 'Apagar Ventilador' : 'Prender Ventilador'}
                         </Button>
+                        <div className="circle" style={{ backgroundColor: circleColor }}></div>
                     </motion.div>
                     <motion.div
                         whileHover={{ scale: 1.1 }}
@@ -67,13 +70,33 @@ const Control = () => {
                         <Button
                             variant="contained"
                             color={sensorTemperatura ? "secondary" : "primary"}
-                            onClick={handleSensorClick}
+                            onClick={() => setSensorTemperatura(!sensorTemperatura)}
                         >
                             {sensorTemperatura ? 'Detener sensor de temperatura' : 'Prender sensor de temperatura'}
                         </Button>
                     </motion.div>
                 </div>
             </Container>
+
+            {/* Diálogo para ajustar la intensidad del foco */}
+            <Dialog open={showDialog} onClose={handleCloseDialog}>
+                <DialogTitle>Ajustar Intensidad del Foco</DialogTitle>
+                <DialogContent>
+                    <Slider
+                        value={intensidadFoco}
+                        onChange={handleChangeIntensidadFoco}
+                        aria-labelledby="continuous-slider"
+                        valueLabelDisplay="auto"
+                        min={0}
+                        max={100}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDialog} color="primary">
+                        Cerrar
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 }
